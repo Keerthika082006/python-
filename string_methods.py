8d35f10f69734783a0ab532deb7ba384d4fab8ca
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-03 18:49:04 +0530

    first commit

---
+++ string_methods.py
@@ -0,0 +1,16 @@
+#string
+
+name=input("enter your name:")
+
+#result=len(name)
+#result=name.find("e")
+result=name.rfind("e")
+#result=name.capitalize()
+#result=name.upper()
+#result=name.lower()
+#result=name.isdigit()
+#result=name.isalpha()
+#result=name.count("/")
+#result=name.replace("/","*")
+print(result)
+
