b3cd80142575065c0fdbbc79e2e2dc184a8c747b
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-05 21:18:30 +0530

    first commit

---
+++ function.py
@@ -0,0 +1,23 @@
+from function1 import sum
+
+#funcion = a block of reusable code
+#       place()after the function name to invoke it
+"""
+def happy_birthday(name,age):
+    print(f"happy birthday to {name}!")
+    print(f"you are {age} years old!")
+    print("happy birthday to u!")
+    print()
+
+happy_birthday("keerthi",21)
+happy_birthday("akilan",22)
+happy_birthday("mithra",23)
+
+def display_invoice(name,amount,duedate):
+    print(f"hello {name}!")
+    print(f"your due amount is {amount:.2f} pay on {duedate}")
+display_invoice("anu",500.20,"22/2")
+"""
+
+result=sum(2,3)
+print(result)
