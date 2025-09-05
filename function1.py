530da8be852ad1e93aca732ddb1cc03e1b760eed
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-05 21:24:24 +0530

    first commit

---
+++ function1.py
@@ -0,0 +1,22 @@
+#no argument with return type
+'''
+def camu():
+    exam1=20
+    exam2=30
+    result=exam1+exam2
+    return result
+r1=camu()
+print("exam result:",r1)
+
+# argument and no return type
+
+def camu(a,b):
+    print("first mrk:",a)
+    print("second mrk:",b)
+camu("raj","bala")
+'''
+def sum(a,b):
+    print(a+b)
+
+result=sum(3,7)
+print=(result)
