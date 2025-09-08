a8e6963c2870c72141f8e65cc032aa7c8845c82c
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-08 22:45:33 +0530

    first commit

---
+++ tuple.py
@@ -0,0 +1,14 @@
+#tuple=() ordered and unchangeable.dupicates ok.faster
+
+fruits=("apple","mango","orange","coconut","coconut")
+
+#print(len(fruits))
+#print("pineapple:" in fruits) 
+#print(fruits[2])
+#print(fruits.index("apple"))
+#print(fruits.count("coconut"))
+fruits[3]="goa"
+#for fruit in fruits:
+ #   print(fruit)
+#print(fruits.clear())
+print(fruits)
