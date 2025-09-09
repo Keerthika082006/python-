d6ee669be2155e30fc98f7de87af01cb48faaa92
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-09 22:45:42 +0530

    first commit

---
+++ typeCasting.py
@@ -0,0 +1,16 @@
+#typecasting=the process of converting a variable from one data to another
+#            str(),int(),float(),bool()
+
+name="keerthi"
+age="keerthi"
+is_student=True
+
+#print(type(age))
+#age=float(age)
+#print(age)
+
+is_student=False
+print(is_student)
+
+age=bool(age)
+print(age)
