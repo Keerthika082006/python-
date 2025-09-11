cb619401c39ffbc46766e505d34cbe6034d44ce6
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-11 13:48:08 +0530

    first commit

---
+++ opps_class_object2.py
@@ -0,0 +1,15 @@
+class student:
+    def  __init__(self):
+        self.name='keerthi'
+        self.grade=12
+
+    def display(self):
+        print(f'im {self.name} my grade is {self.grade}')
+
+s1=student()
+s2=student()
+s3=student()
+
+s1.display()
+s2.display()
+s3.display()
