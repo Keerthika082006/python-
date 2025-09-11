17d3e98b510d1597704f6ada43841da98da49d95
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-11 13:59:27 +0530

    first commit

---
+++ opps-_single_level_inheritance.py
@@ -0,0 +1,13 @@
+class dad:
+    def house(self):
+        print("house colour red")
+
+class son(dad):
+    def house1(self):
+        print("house colour pink")
+    def house(self):
+        print("house colour green")
+        
+o1=son()
+o1.house()
+o1.house1()
