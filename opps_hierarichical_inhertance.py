b54d871b2e2729d3f4dd4e1c6de3ff337976ebc7
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-14 23:18:39 +0530

    first commit

---
+++ opps_hierarichical_inhertance.py
@@ -0,0 +1,20 @@
+class dad:
+    def father(self):
+        print('house')
+class son1(dad):
+    def mano(self):
+        print('factory')
+class son2(dad):
+    def rahul(self):
+        print('market')
+
+s1=son1()
+s1.mano()
+s1.father()
+'''
+   (or)
+
+s1=son2()
+s1.rahul()
+s1.father()
+'''
