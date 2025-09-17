9db5526513b348364cd2d2e32c7850b643d18447
Keerthi082006 <dkeerthika1808@gmail.com>
2025-09-17 22:28:59 +0530

    first commit

---
+++ madlibsgame.py
@@ -0,0 +1,12 @@
+#madlibs game
+#word game where you create a story
+#by filling in blanks with random words
+
+adjective1=input("enter an adjective:")
+noun1=input("enter a noun(person,place,thing):")
+adjective2=input("enter an adjective:")
+
+print(f"yesterday i went to a {adjective1} temple")
+print(f"in the temple i saw my friend {noun1}")
+print(f"{noun1} was very happy on seeing mee")
+print(f"we both went to {adjective2}")
