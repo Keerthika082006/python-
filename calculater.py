03d33f0a99c421a4f3bb2b6384fd6202823dc572
Keerthika <dkeerthika1808@gmail.com>
2025-09-20 20:28:25 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,24 @@
+print('the calculater')
+
+def add(a,b):
+    c=a+b
+    print("result:",c)
+def sub(a,b):
+    c=a-b
+    print("result:",c)
+    
+def mul(a,b):
+    c=a*b
+    print("result:",c)
+def div(a,b):
+    c=a/b
+    print("result:",c)
+    
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
+print("your ans is verified")
