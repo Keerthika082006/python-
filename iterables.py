313b56328fe93620aa9a7c30742e95254c128a08
Keerthika <dkeerthika1808@gmail.com>
2025-09-20 20:30:00 +0530

    Add files via upload
    basics of dictionary

---
+++ iterables.py
@@ -0,0 +1,8 @@
+#iterables= an oject or collections that can return its element one at a time,
+#           allowing it to ne iterated over in a loop
+#           it can be list,tuple,set dict
+
+number=[1,2,3,4,5,6]
+
+for x in number:
+    print(x)
